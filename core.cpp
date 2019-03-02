2142449ca431201ddf307ff6aa4fda95749fdc28
Nguyễn Xuân Hiến <hiennguyen9874@gmail.com>
2019-03-02 17:56:38 +0700

    Them chuc nang moi

--- core.cpp
+++ core.cpp
@@ -32,6 +32,26 @@ bool __isExiting = false;		/// this variable should be turn on when the program 
 struct Item
 {
 	string str1, str2;
+
+	bool operator>(Item &a)
+	{
+		return this->str1 > a.str1;
+	}
+
+	bool operator<(Item &a)
+	{
+		return this->str1 < a.str1;
+	}
+
+	bool operator>=(Item &a)
+	{
+		return this->str1 >= a.str1;
+	}
+
+	bool operator<=(Item &a)
+	{
+		return this->str1 <= a.str1;
+	}
 };
 
 struct Lst
@@ -45,6 +65,25 @@ struct Lst
 	double introTime;
 };
 Lst ret;
+
+enum MyType
+{
+	INTRO,
+	WELCOME,
+	MENU
+};
+
+void sort_List_Item(Item lst[], int len)
+{
+	for (int i = 0; i < len; i++)
+		for (int j = i; j < len; j++)
+			if (lst[i] > lst[j])
+			{
+				Item temp = lst[i];
+				lst[i] = lst[j];
+				lst[j] = temp;
+			}
+}
 
 ///--------------------------------------------------------------------
 /**
@@ -78,15 +117,20 @@ void LoadConfiguration()
 	// TODO: write code to load data from the configuration file
 	ifstream inFile("conf.json");
 	string line;
-	int type = 0;
+	MyType type = INTRO;
 	while (true)
 	{
 		getline(inFile, line);
 		stringstream ss(line);
 		string str1;
 		ss >> str1;
-		if (str1 == "{" || str1 == "}," || str1 == "")
+		if (str1 == "{" || str1 == "")
 		{
+			continue;
+		}
+		else if (str1 == "},")
+		{
+			type = INTRO;
 			continue;
 		}
 		else if (str1 == "}")
@@ -107,12 +151,12 @@ void LoadConfiguration()
 
 		if (str1 == "WelcomeText" && str0 == "{")
 		{
-			type = 1;
+			type = WELCOME;
 			continue;
 		}
 		else if (str1 == "Menu" && str0 == "{")
 		{
-			type = 2;
+			type = MENU;
 			continue;
 		}
 		else if (str1 == "IntroTime")
@@ -121,25 +165,27 @@ void LoadConfiguration()
 			continue;
 		}
 
-		if (type == 0)
+		if (type == INTRO)
 		{
 			ret.intro[ret.lenIntro].str1 = str1;
 			ret.intro[ret.lenIntro].str2 = str2;
 			ret.lenIntro++;
 		}
-		else if (type == 1)
+		else if (type == WELCOME)
 		{
 			ret.wel[ret.lenWel].str1 = str1;
 			ret.wel[ret.lenWel].str2 = str2;
 			ret.lenWel++;
 		}
-		else if (type == 2)
+		else if (type == MENU)
 		{
 			ret.menu[ret.lenMenu].str1 = str1;
 			ret.menu[ret.lenMenu].str2 = str2;
 			ret.lenMenu++;
 		}
 	}
+	sort_List_Item(ret.wel, ret.lenWel);
+	sort_List_Item(ret.menu, ret.lenMenu);
 	inFile.close();
 }
 
@@ -150,13 +196,13 @@ void LoadMenu()
 	{
 		cout << ret.wel[i].str2 << endl;
 	}
-	sleep(ret.introTime);
+	// sleep(ret.introTime);
 }
 
 void DisplayMenu()
 {
 	// TODO: Display the menu loaded from configuration file
-	system("clear");
+	// system("clear");
 	for (int i = 0; i < ret.lenMenu; i++)
 	{
 		cout << i + 1 << ". " << ret.menu[i].str2 << endl;
@@ -176,6 +222,54 @@ bool isinteger(string const &n) noexcept
 	return false;
 }
 
+void get(string _str)
+{
+	if (_str == "Introduction")
+	{
+		for (int i = 0; i < ret.lenIntro; i++)
+		{
+			cout << ret.intro[i].str1 << ": " << ret.intro[i].str2 << endl;
+		}
+		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
+	}
+	else if (_str == "Login")
+	{
+		cout << "Chức năng đang trong quá trình phát triển!" << endl;
+		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
+	}
+	else if (_str == "Registration")
+	{
+		cout << "Chức năng đang trong quá trình phát triển!" << endl;
+		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
+	}
+	else if (_str == "Help")
+	{
+		cout << "Bạn muốn giúp đỡ, vui lòng liên hệ thầy Dũng :v" << endl;
+		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
+	}
+	else if (_str == "Exit")
+	{
+		cout << "exiting..." << endl;
+		__isExiting = true;
+	}
+	else
+	{
+		cout << "Chức năng không tồn tại trong hệ thống, vui lòng liên hệ K' Đen để thêm chức năng này!" << endl;
+		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
+	}
+	
+}
+
 void ProcessUserChoice()
 {
 	// TODO: Read user input and process
@@ -185,6 +279,8 @@ void ProcessUserChoice()
 	{
 		cout << "Invalid input, please input an integer number." << endl;
 		sleep(ret.introTime);
+		system("clear");
+		LoadMenu();
 	}
 	else
 	{
@@ -193,11 +289,12 @@ void ProcessUserChoice()
 		{
 			cout << "Please select a number from 1 to 5." << endl;
 			sleep(ret.introTime);
+			system("clear");
+			LoadMenu();
 		}
 		else
 		{
-			cout << ret.menu[num - 1].str2 << endl;
-			__isExiting = true;
+			get(ret.menu[num-1].str2);
 		}
 	}
 }
